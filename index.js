9ca7e2dd36949ab9652ef0ca716b170e265c0aca
Maria Dayekh <mariaaldayekh@gmail.com>
2022-11-27 16:42:13 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName= 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = "Muslem";
+
+function upperCaseCustomerName(){
+return customerName.toUpperCase;
+}
+
+function setBestCustomer (){
+var bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    var bestCustomer = 'jana';
+}
+
+function leastFavoriteCustomer (){
+    return leastFavoriteCustomer;
+}
+
+function changeLeastFavoriteCustomer (){
+    const leastFavoriteCustomer = 'Ammar';
+}
